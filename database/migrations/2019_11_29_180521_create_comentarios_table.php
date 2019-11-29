3522682742651304a0db95709eaf9c96e0c8b367
MatiasMart <matiasmartinelli12@gmail.com>
2019-11-29 15:16:18 -0300

    ya cree todas las migraciones

---
+++ database/migrations/2019_11_29_180521_create_comentarios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('id_post');
+            $table->foreign('id_post')->references('id')->on('post');
+            $table->unsignedInteger('id_usuario');
+            $table->foreign('id_usuario')->references('id')->on('users');
+            $table->text('comentario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comentarios');
+    }
+}
